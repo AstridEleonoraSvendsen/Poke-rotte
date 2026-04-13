1ce7f8a48e91dab80348b141c2855b5456b093eb
AstridEleonoraSvendsen <119928972+AstridEleonoraSvendsen@users.noreply.github.com>
2026-04-13 19:15:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { Toaster } from 'sonner'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
@@ -52,6 +53,7 @@ export default function RootLayout({
         {/* ------------------------------------ */}
 
         {children}
+        <Toaster position="top-center" richColors />
         <Analytics />
       </body>
     </html>
